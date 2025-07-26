cd10906843bcff7c31f5cc9af05a3f241e86add2
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-07-26 21:01:07 +0900

    feat: DIP 지키기 위해 AppConfig 의존성 주입, Test 코드도 수정함
    - AppConfig 로 관심사 확실히 분리
    - AppConfig 는 구체 클래스를 선택해서 애플리케이션이 어떻게 동작해야 할지 전체 구성을 책임진다.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository; // 추상화에 의존
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    } // 생성자를 통해서 구현체에 뭐가 들어갈지 정함
 
     @Override
     public void join(Member member) {
