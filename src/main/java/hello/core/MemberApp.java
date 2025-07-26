a4c57a1e3657c4917fb8e80b3f2e0bc3a88e2aa0
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-07-26 23:46:04 +0900

    feat: DI 컨테이너 생성

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,21 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig 환경 설정 정보 가지고,
+        // 스프링이 @Bean 붙은 걸 스프링 빈에 객체 생성한 걸 다 넣어서 관리해줌
+        // 직접 찾아오다가 이제 applicationContext에서 가져옴
+        applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
