54ece13f6370017f304334a0b0e3c5c7bea567e1
jonadan <0620jonathan@gmail.com>
2025-07-28 10:43:37 +0900

    feat: 싱글톤 - 상태를 유지할 경우 발생하는 문제점 해결

--- src/test/java/hello/core/singleton/StateFulServiceTest.java
+++ src/test/java/hello/core/singleton/StateFulServiceTest.java
@@ -16,15 +16,15 @@ class StateFulServiceTest {
         StateFulService stateFulService2 = ac.getBean(StateFulService.class);
 
         // ThreadA : A 사용자 10000원 주문
-        stateFulService1.order("userA", 10000);
+        int userAPrice = stateFulService1.order("userA", 10000);
         // ThreadB : B 사용자 20000원 주문
-        stateFulService1.order("userb", 20000);
+        int userBPrice = stateFulService1.order("userb", 20000);
 
         // ThreadA: 사용자 A 주문 금액 조회
-        int price = stateFulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = stateFulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        Assertions.assertThat(stateFulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(stateFulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
