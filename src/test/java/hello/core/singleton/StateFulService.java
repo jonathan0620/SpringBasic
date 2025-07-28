9ed102792e98d74ab31af1e25f6a5933adfa1e7b
jonadan <0620jonathan@gmail.com>
2025-07-28 11:18:16 +0900

    feat: void -> int로 수정

--- src/test/java/hello/core/singleton/StateFulService.java
+++ src/test/java/hello/core/singleton/StateFulService.java
@@ -3,7 +3,7 @@ package hello.core.singleton;
 public class StateFulService {
 //    private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
 //        this.price = price; // 여기가 문제
         return price;
