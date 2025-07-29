6060852a2a9874a049e5b9ec9d4e8fcffdb789f4
jonadan <0620jonathan@gmail.com>
2025-07-29 10:23:02 +0900

    feat: @Component와 @Autowired 어노테이션 추가
    - 스프링 빈 주입하려는 곳에 @Component
    - 생성자 위에 @Autowired

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository; // 추상화에 의존
 
+    @Autowired // 자동 의존관계 주입 ac.getBean(MemberRepository.class) 이게 자동으로 들어간다고 보면 됨
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     } // 생성자를 통해서 구현체에 뭐가 들어갈지 정함
