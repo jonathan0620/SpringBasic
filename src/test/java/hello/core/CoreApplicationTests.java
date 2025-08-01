d08498ce7e4d78511033c81f9cf38be0bffbd989
jonadan <0620jonathan@gmail.com>
2025-08-01 10:46:16 +0900

    feat: request scope 예제 개발

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,14 @@
 package hello.core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplicationTests.class, args);
 	}
 
 }
